376a658ac0120d42a145ad8a43434ea171d2451b
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2011-04-23 23:57:38 -0500

    Add license information

--- receive.cpp
+++ receive.cpp
@@ -1,3 +1,25 @@
+/**********************************************************************************/
+/*      Copyright © 2011, Kumar Appaiah, Radha Krishna Ganti, Kannan Srinivasan  */
+/*      The University of Texas at Austin                                         */
+/*                                                                                */
+/*      This file is part of Simple OFDM Modem.                                   */
+/*                                                                                */
+/*      Simple OFDM Modem is free software: you can redistribute it and/or        */
+/*      modify it under the terms of the GNU General Public License as            */
+/*      published by the Free Software Foundation, either version 3 of the        */
+/*      License, or (at your option) any later version.                           */
+/*                                                                                */
+/*      Simple OFDM Modem is distributed in the hope that it will be              */
+/*      useful, but WITHOUT ANY WARRANTY; without even the implied                */
+/*      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.          */
+/*      See the GNU General Public License for more details.                      */
+/*                                                                                */
+/*      You should have received a copy of the GNU General Public License         */
+/*      along with Simple OFDM Modem.  If not, see                                */
+/*      <http://www.gnu.org/licenses/>.                                           */
+/*                                                                                */
+/**********************************************************************************/
+
 #include "parameters.hpp"
 #include "receive.hpp"
 #include "unwrap.hpp"
